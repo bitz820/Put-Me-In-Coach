7872e9a34d8d90e815693c55d7435c7d6d997b76
Tosie <minnaji@outlook.com>
2022-04-18 12:35:25 -0400

    search bar

---
+++ src/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SearchBar({placeholder,data}) {
+    return (
+        <div className="search">
+            <div className="searchInputs">
+            <input type="text" placeholder={placeholder}/>
+            <div className="searchIcon"> </div>
+            </div>
+            <div className="dataResult"></div>
+        </div>
+    )
+
+}
+
+export default SearchBar;
