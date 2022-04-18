fdddaffefc687db6f9d39e742340a1a08d762172
Mark <mark.n.bacon.jr@gmail.com>
2022-04-18 11:06:30 -0400

    modified DB, started basic stylings, created cards for teams, imported images coniditionally rendered based on team size

--- src/GameCard.js
+++ src/GameCard.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./GameCard.css"
+import logo3 from "./3Team.png"
+import logo5 from "./5Team.png"
 
 
-function GameCard({data}) {
-    console.log(data)
-    const {id, name, type, playersNeeded, color, date, court} = data
-  return (
-    <card>
-        <h1>Team Name: {name}</h1>
-        <h3>Game Type: {type} </h3>
-        {/* Game Type should also be stateful, and card styled differently (ternary) */}
-        <h4>Number of Players needed: {playersNeeded} </h4>
-        {/* Players needed should be stateful, if full we should indicated Game Full in green, if needed players, we should indicate in red! */}
-        <p>Team Color: {color} </p>
-        <p>Court Number: {court}</p>
-        <p>Game Date: {date}</p>
-    </card>
-  )
+function GameCard({ data }) {
+    // console.log(data)
+    console.log(logo3, logo5)
+    // const [isThree, setIsThree] = useState(true)
+    const { id, name, type, playersNeeded, color, date, court } = data
+    const teamTypeStyle = type === "3v3" ? logo3 : logo5
+
+    return (
+
+
+        <div className='card-container'>
+            <div className='card-title'>
+                <h1>
+                    Team Name: {name}
+                </h1>
+            </div>
+            <div className='image-container'>
+                <img src={teamTypeStyle} alt="type of game" />
+            </div>
+            <div className='card-body'>
+
+                <h3>Game Type: {type} </h3>
+                {/* Game Type should also be stateful, and card styled differently (ternary) */}
+                <h4>Players Needed: {playersNeeded} </h4>
+                {/* Players needed should be stateful, if full we should indicated Game Full in green, if needed players, we should indicate in red! */}
+                <p>Team Color: {color} </p>
+                <p>Court Number: {court}</p>
+                <p>Game Date: {date}</p>
+            </div>
+        </div>
+    )
 }
 
 export default GameCard
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import GameCard from './GameCard'
+import "./Home.css"
 
 function Home({data}) {
   console.log(data)
 
   const renderTeams = data.map(team => <GameCard key={team.id} data={team}/>)
   return (
-    <div>
+    <div className='home-style'>
       <h1>Welcome to Put Me In, Coach!</h1>
       <h3>Your #1 Source for last minute substitutions and available pickup games!</h3>
       {renderTeams}
