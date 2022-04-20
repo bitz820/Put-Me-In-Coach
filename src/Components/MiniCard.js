dc52c185f811b2071dfdf7ec6677256bf6dba5d2
Tosie <minnaji@outlook.com>
2022-04-20 09:14:08 -0400

    added join button to modal, still need to redirect

--- src/Components/MiniCard.js
+++ src/Components/MiniCard.js
@@ -47,6 +47,7 @@ function MiniCard({ data }) {
                     </Modal.Body>
                     <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="primary" onClick={handleClose}>Join</Button>
                     </Modal.Footer>
                     
                 </Modal>
